ad7478149af5d7f52a70b4f92399b2756c7a0d4e
Guilherme Albuquerque <guilherme.albuq2512@gmail.com>
2026-02-10 16:04:14 -0300

    first commit

---
+++ src/test/java/dev/java10x/cadastroPessoa/CadastroPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.cadastroPessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
